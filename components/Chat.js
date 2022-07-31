676bbdf907eb72dcfa63690c466653fa2938855f
Jimmy Chen <jimmychen9785@gmail.com>
2022-07-30 20:58:12 -0700

    connected chat app with firestore

--- components/Chat.js
+++ components/Chat.js
@@ -2,42 +2,112 @@ import React from 'react';
 import { View } from 'react-native';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
     };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyBqnY2xSfgrbGxAAKeOW4qDhrUYlnLVRSI',
+        authDomain: 'chat-e35b0.firebaseapp.com',
+        projectId: 'chat-e35b0',
+        storageBucket: 'chat-e35b0.appspot.com',
+        messagingSenderId: '184320131977',
+        appId: '1:184320131977:web:888193e64d9054b8401758',
+        measurementId: 'G-BSY3Q8NGV5',
+      });
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection('messages');
   }
 
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // Go through each document
+    querySnapshot.forEach((doc) => {
+      // Get the QueryDocumentsSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text || '',
+        createdAt: data.createdAt.toDate(),
+        user: data.user,
+      });
+    });
+  };
+
+  // runs with component first gets mounted
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello!',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
+    let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+
+    // Reference to load messages via Firebase
+    this.referenceChatMessages = firebase.firestore().collection('messages');
+
+    // Authenticates user via Firebase
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: 'https://placeimg.com/140/140/any',
         },
-        {
-          _id: 2,
-          text: 'Welcome to Chat1',
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
+      });
+      this.referenceMessagesUser = firebase
+        .firestore()
+        .collection('messages')
+        .where('uid', '==', this.state.uid);
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  // run when component unmounts
+  componentWillUnmount() {
+    this.authUnsubscribe();
+    this.unsubscribe();
+  }
+
+  // adding messages to firestore
+  addMessages() {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: message.user,
     });
   }
 
   // sending message
   onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessages();
+      }
+    );
   }
 
   // change styles of bubble
@@ -46,9 +116,6 @@ export default class Chat extends React.Component {
       <Bubble
         {...props}
         wrapperStyle={{
-          left: {
-            backgroundColor: '#fafafa',
-          },
           right: {
             backgroundColor: '#2d7ecf',
           },
@@ -58,14 +125,12 @@ export default class Chat extends React.Component {
   }
 
   render() {
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
-    const { bgColor } = this.props.route.params;
+    let { color, name } = this.props.route.params;
 
     return (
       <View
         style={{
-          backgroundColor: bgColor,
+          backgroundColor: color,
           flex: 1,
         }}
       >
@@ -74,7 +139,9 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.user._id,
+            name: name,
+            avatar: this.state.user.avatar,
           }}
         />
       </View>
